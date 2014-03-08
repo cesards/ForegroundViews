cfa8e7bd9e40b8995153f7527d293b3d513cc1c4
César Díez Sánchez <cesaryomismo@gmail.com>
2014-03-08 18:46:15 +0100

    .gitignore is now working

--- demo/build/res/all/debug/raw/directions_defaults.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE IF NOT EXISTS directions (_id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, place TEXT, distance REAL, latitude REAL, longitude REAL, image_path TEXT);
-INSERT INTO directions (name, place, latitude, longitude) VALUES ('Great Waterfall', 'Bogus Demo', '-25.499376', '-54.108753');
-INSERT INTO directions (name, place, latitude, longitude) VALUES ('Lots of Mushrooms', 'Bogus Demo', '41.827299', '2.405355');
-INSERT INTO directions (name, place, latitude, longitude) VALUES ('Mountain Shelter', 'Bogus Demo', '-32.655563', '-70.010834');
-INSERT INTO directions (name, place, latitude, longitude) VALUES ('Buried Treasure', 'Bogus Demo', '-42.611211', '146.37337');
-INSERT INTO directions (name, place, latitude, longitude) VALUES ('Meeting in the Sea', 'Bogus Demo', '39.807283', '4.285083');
-INSERT INTO directions (name, place, latitude, longitude) VALUES ('Famous Shipwreck', 'Bogus Demo', '59.324914', '18.050609');
-INSERT INTO directions (name, place, latitude, longitude) VALUES ('Where I parked', 'Bogus Demo', '37.748288', '-119.551703');
-INSERT INTO directions (name, place, latitude, longitude) VALUES ('Our Campsite', 'Bogus Demo', '46.763494', '-93.257071');
